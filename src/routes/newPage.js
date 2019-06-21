a5a000962370946b1dd4e7393f266516d4a9c226
p_dhuizou <p_dhuizou@tencent.com>
2019-06-21 11:19:25 +0800

    add

--- src/routes/newPage.js
+++ src/routes/newPage.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { List, Button, NavBar, Toast, DatePicker, Flex } from "antd-mobile";
+import { List, Button, NavBar, Toast, Flex } from "antd-mobile";
 import { insertUser } from "../services/service";
 import styles from "../assets/css/home.less";
-import { createHashHistory } from 'history';
+import { createHashHistory } from "history";
 import { isAuthenticated } from "../utils/session";
+import DatePicker from "react-mobile-datepicker";
 class newPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       testName: "",
       gender: "",
-      date: ""
+      date: "",
+      isOpen: false
     };
   }
   componentDidMount() {
-    document.title='创建用户';
+    document.title = "创建用户";
     const cookies = isAuthenticated();
     if (cookies) {
       const cookiesInfo = JSON.parse(cookies);
-      this.openid =  cookiesInfo.openid;
+      this.openid = cookiesInfo.openid;
     }
   }
 
@@ -34,25 +36,36 @@ class newPage extends React.Component {
         gender: this.state.gender,
         birthday: this.state.date,
         type: "WX",
-        openid:  this.openid,
+        openid: this.openid,
         age: age
       }).then(data => {
-        if(data.data.status === 0) {
-          Toast.info('创建成功');
+        if (data.data.status === 0) {
+          Toast.info("创建成功");
           createHashHistory().goBack();
-        } else{
-          Toast.info(data.data.message)
+        } else {
+          Toast.info(data.data.message);
         }
       });
     }
+  };
+  dateToString = date => {
+    var year = date.getFullYear();
+    var month = (date.getMonth() + 1).toString();
+    var day = date.getDate().toString();
+    if (month.length === 1) {
+      month = "0" + month;
+    }
+    if (day.length === 1) {
+      day = "0" + day;
+    }
+    var dateTime = year + "-" + month + "-" + day;
+    return dateTime;
   };
   render() {
     return (
       <div className={styles["animate-route"]} style={{ background: " white" }}>
         {/* <NavBar mode="dark">创建用户</NavBar> */}
-        <div style={{ textAlign: "center",fontSize: "20px" }}>
-          你的姓名
-        </div>
+        <div style={{ textAlign: "center", fontSize: "20px" ,paddingTop:20 }}>你的姓名</div>
         <div>
           <input
             style={{
@@ -120,17 +133,21 @@ class newPage extends React.Component {
             paddingTop: 20
           }}
         >
-          <DatePicker
-            minDate= {new Date('1900-1-1')}
-            maxDate={new Date('2020-1-1')}
-            width="300px"
-            mode="date"
-            extra="请选择"
-            value={this.state.date}
-            onChange={date => this.setState({ date })}
+          <List.Item
+            onClick={() => this.setState({ isOpen: true })}
+            extra={this.state.date && this.dateToString(this.state.date)}
           >
-            <List.Item>您的生日</List.Item>
-          </DatePicker>
+            您的生日
+          </List.Item>
+          <DatePicker
+          theme={"ios"}
+            date={this.state.date}
+            isOpen={this.state.isOpen}
+            onCancel={() => this.setState({ isOpen: false })}
+            onSelect={date => {
+              this.setState({ date: date, isOpen: false });
+            }}
+          />
         </div>
 
         <div style={{ marginTop: 16 }}>
